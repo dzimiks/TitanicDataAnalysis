1fd08db475350b2988397c2dae6bd3fc088ddec4
dzimiks <vana997@gmail.com>
2018-04-21 18:05:41 +0200

    Train model with K-Fold Cross-Validation

--- TitanicDataAnalysis.R
+++ TitanicDataAnalysis.R
@@ -383,14 +383,96 @@ rf.7 <- randomForest(x = rf.train.7, y = rf.label, importance = TRUE, ntree = 10
 rf.7
 varImpPlot(rf.7)
 
+# Subset our test records and features
+test.submit.df <- data.combined[892:1309, c("Pclass", "Title", "Family.size")]
 
+# Make predictions 
+rf.5.preds <- predict(rf.5, test.submit.df)
+table(rf.5.preds)
 
+# Write out a CSV file for submission to Kaggle
+submit.df <- data.frame(PassengerId = rep(892:1309), Survived = rf.5.preds)
 
+write.csv(submit.df, file = "RF_SUB_20180421_1.csv", row.names = FALSE)
 
+# First try submission scores 0.79425
+# Let's look into cross-validation using the caret package to see if we can get
+# more accurate estimates
+library(caret)
+library(doSNOW)
 
+# Leverage caret to create 100 total folds, but ensure that the ratio of those
+# that survived and perished in each fold matches the overall training set. This
+# is known as stratified cross validation and generally provides better results.
+set.seed(2348)
+cv.10.folds <- createMultiFolds(rf.label, k = 10, times = 10)
 
+# Check stratification
+table(rf.label)
+table(rf.label[cv.10.folds[[33]]])
 
+# Set up caret's trainControl object per above.
+ctrl.1 <- trainControl(method = "repeatedcv", number = 10, repeats = 10, index = cv.10.folds)
 
+# Set up doSNOW package for multi-core training. This is helpful as we're going
+# to be training a lot of trees.
+cl <- makeCluster(6, type = "SOCK")
+registerDoSNOW(cl)
 
+# Set seed for reproducibility and train
+set.seed(34324)
+rf.5.cv.1 <- train(x = rf.train.5, y = rf.label, method = "rf", tuneLength = 3,
+                   ntree = 1000, trControl = ctrl.1)
 
+# Shutdown cluster
+stopCluster(cl)
 
+# Check out results
+rf.5.cv.1
+
+# The above is only slightly more pessimistic than the rf.5 OOB prediction, but
+# not pessimistic enough. Let's try 5-fold CV repeated 10 times.
+set.seed(5983)
+cv.5.folds <- createMultiFolds(rf.label, k = 5, times = 10)
+
+# Set up caret's trainControl object per above.
+ctrl.2 <- trainControl(method = "repeatedcv", number = 5, repeats = 10, index = cv.5.folds)
+
+# Set up doSNOW package for multi-core training. This is helpful as we're going
+# to be training a lot of trees.
+cl <- makeCluster(6, type = "SOCK")
+registerDoSNOW(cl)
+
+# Set seed for reproducibility and train
+set.seed(89472)
+rf.5.cv.2 <- train(x = rf.train.5, y = rf.label, method = "rf", tuneLength = 3,
+                   ntree = 1000, trControl = ctrl.2)
+
+# Shutdown cluster
+stopCluster(cl)
+
+# Check out results
+rf.5.cv.2
+
+# 5-fold CV isn't better. Move to 3-fold CV repeated 10 times.
+set.seed(37596)
+cv.3.folds <- createMultiFolds(rf.label, k = 3, times = 10)
+
+# Set up caret's trainControl object per above.
+ctrl.3 <- trainControl(method = "repeatedcv", number = 3, repeats = 10, index = cv.3.folds)
+
+# Set up doSNOW package for multi-core training. This is helpful as we're going
+# to be training a lot of trees.
+cl <- makeCluster(6, type = "SOCK")
+registerDoSNOW(cl)
+
+# Set seed for reproducibility and train
+set.seed(94622)
+rf.5.cv.3 <- train(x = rf.train.5, y = rf.label, method = "rf", tuneLength = 3,
+                   ntree = 1000, trControl = ctrl.3)
+
+# Shutdown cluster
+stopCluster(cl)
+
+# Check out results
+rf.5.cv.3
