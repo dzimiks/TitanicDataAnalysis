10ae1a83be0b29bf5fffc6e3a0f6c662fa8dd9f7
dzimiks <vana997@gmail.com>
2018-04-25 03:03:50 +0200

    Make some decision trees to investigate correlation between Ticket.party.size and Avg.fare

--- TitanicDataAnalysis.R
+++ TitanicDataAnalysis.R
@@ -320,9 +320,9 @@ ggplot(data.combined[1:891,], aes(x = Embarked, fill = Survived)) +
   ylim(0, 300) +
   labs(fill = "Survived")
 
-# ====================
-# Exploratory Modeling
-# ====================
+# ======================
+# Exploratory Modeling 1
+# ======================
 
 library(randomForest)
 
@@ -382,6 +382,10 @@ set.seed(1234)
 rf.7 <- randomForest(x = rf.train.7, y = rf.label, importance = TRUE, ntree = 1000)
 rf.7
 varImpPlot(rf.7)
+
+# ================
+# Cross Validation
+# ================
 
 # Subset our test records and features
 test.submit.df <- data.combined[892:1309, c("Pclass", "Title", "Family.size")]
@@ -476,3 +480,236 @@ stopCluster(cl)
 
 # Check out results
 rf.5.cv.3
+
+# ======================
+# Exploratory Modeling 2
+# ======================
+
+# Let's use a single decision tree to better understand what's going on with our
+# features. Obviously Random Forest are far more powerful than single trees,
+# but single trees have the advantage of being easier to understand.
+
+# Load packages
+library(rpart)
+library(rpart.plot)
+
+# Create utility function
+rpart.cv <- function(seed, training, labels, ctrl) {
+  cl <- makeCluster(6, type = "SOCK")
+  registerDoSNOW(cl)
+  
+  set.seed(seed)
+  
+  # Levarage formula interface for training
+  rpart.cv <- train(x = training, y = labels, method = "rpart", tuneLength = 30, trControl = ctrl)
+  
+  # Shutdown cluster
+  stopCluster(cl)
+  
+  return (rpart.cv)
+}
+
+# Grab features 
+features <- c("Pclass", "Title", "Family.size")
+rpart.train.1 <- data.combined[1:891, features]
+
+# Run CV and check out results
+rpart.1.cv.1 <- rpart.cv(94622, rpart.train.1, rf.label, ctrl.3)
+rpart.1.cv.1
+
+# Plot
+prp(rpart.1.cv.1$finalModel, type = 0, extra = 1, under = TRUE)
+
+# The plot bring out some interesting lines of investigation. Namely:
+#     1 - Titles of "Mr." and "Other" are predicted to perish at an
+#         overall accuracy rate of 83.2%.
+#     2 - Titles of "Master.", "Miss." & "Mrs." in 1st & 2nd class
+#         are predicted to survive at an overall accuracy rate of 94.9%.
+#     3 - Titles of "Master.", "Miss." & "Mrs." in 3rd class with
+#         family sizes equal to 5, 6, 8 & 11 are predicted to perish
+#         with 100% accuracy.
+#     4 - Titles of "Master.", "Miss." & "Mrs." in 3rd class with 
+#         family sizes not equal to 5, 6, 8 & 11 are predicted to
+#         survive with 59.6% accuracy.
+
+# Both rpart and rf confirm that title is important, let's investigate further
+table(data.combined$Title)
+
+# Parse out last Name and Title
+data.combined[1:25, "Name"]
+
+name.splits <- str_split(data.combined$Name, ",")
+name.splits[1]
+last.names <- sapply(name.splits, "[", 1)
+last.names[1:10]
+
+# Add last names to dataframe in case we find it useful later
+data.combined$Last.name <- last.names
+
+# Now for titles
+name.splits <- str_split(sapply(name.splits, "[", 2), " ")
+titles <- sapply(name.splits, "[", 2)
+unique(titles)
+
+# What's up with a Title of "the"?
+data.combined[which(titles == "the"),]
+
+# Re-map titles to be more exact
+titles[titles %in% c("Dona.", "the")] <- "Lady."
+titles[titles %in% c("Ms.", "Mlle")] <- "Miss."
+titles[titles == "Mme."] <- "Mrs."
+titles[titles %in% c("Jonkheer.", "Don.")] <- "Sir."
+titles[titles %in% c("Col.", "Capt.", "Major.")] <- "Officer"
+table(titles)
+ 
+# Make Title a factor
+data.combined$New.title <- as.factor(titles)
+
+# Visualize new version of Title
+ggplot(data.combined[1:891,], aes(x = New.title, fill = Survived)) +
+  geom_bar() +
+  facet_wrap(~ Pclass) +
+  ggtitle("Survival Rates for New.title by Pclass")
+
+# Collapse titles based on visual analysis
+indexes <- which(data.combined$New.title == "Lady.")
+data.combined$New.title[indexes] <- "Mrs."
+
+indexes <- which(data.combined$New.title == "Dr." |
+                 data.combined$New.title == "Rev." |
+                 data.combined$New.title == "Sir." |
+                 data.combined$New.title == "Officer")
+data.combined$New.title[indexes] <- "Mr."
+
+# Visualize
+ggplot(data.combined[1:891,], aes(x = New.title, fill = Survived)) +
+  geom_bar() +
+  facet_wrap(~ Pclass) +
+  ggtitle("Survival Rates for Collapsed New.title by Pclass")
+
+# Grab features
+features <- c("Pclass", "New.title", "Family.size")
+rpart.train.2 <- data.combined[1:891, features]
+
+# Run CV and check out results
+rpart.2.cv.1 <- rpart.cv(94622, rpart.train.2, rf.label, ctrl.3)
+rpart.2.cv.1
+
+# Plot
+prp(rpart.2.cv.1$finalModel, type = 0, extra = 1, under = TRUE)
+
+# Dive in on 1st class "Mr."
+indexes.first.mr <- which(data.combined$New.title == "Mr." & data.combined$Pclass == "1")
+first.mr.df <- data.combined[indexes.first.mr, ]
+summary(first.mr.df)
+
+# One female?
+first.mr.df[first.mr.df$Sex == "female",]
+
+# Update New.title feature 
+indexes <- which(data.combined$New.title == "Mr." &
+                 data.combined$Sex == "female")
+data.combined$New.title[indexes] <- "Mrs."
+
+# Any other gender slip-ups?
+length(which(data.combined$Sex == "female" &
+            (data.combined$New.title == "Master." |
+             data.combined$New.title == "Mr.")))
+
+# Refresh data frame
+indexes.first.mr <- which(data.combined$New.title == "Mr." & data.combined$Pclass == "1")
+first.mr.df <- data.combined[indexes.first.mr, ]
+
+# Let's look at surviving 1st class "Mr."
+summary(first.mr.df[first.mr.df$Survived == "1",])
+View(first.mr.df[first.mr.df$Survived == "1",])
+
+# Take a look at some of the high fares
+indexes <- which(data.combined$Ticket == "PC 17755" |
+                 data.combined$Ticket == "PC 17611" |
+                 data.combined$Ticket == "113760")
+View(data.combined[indexes,])
+
+# Visualize survival rates for 1st class "Mr." by fare
+ggplot(first.mr.df, aes(x = Fare, fill = Survived)) +
+  geom_density(alpha = 0.5) +
+  ggtitle("1st Class 'Mr.' Survival Rates by Fare")
+
+# Engineer features based on all the passengers with the same Ticket
+ticket.party.size <- rep(0, nrow(data.combined))
+avg.fare <- rep(0.0, nrow(data.combined))
+tickets <- unique(data.combined$Ticket)
+
+for (i in 1:length(tickets)) {
+  current.ticket <- tickets[i]
+  party.indexes <- which(data.combined$Ticket == current.ticket)
+  current.avg.fare <- data.combined[party.indexes[1], "Fare"] / length(party.indexes)
+  
+  for (k in 1:length(party.indexes)) {
+    ticket.party.size[party.indexes[k]] <- length(party.indexes)
+    avg.fare[party.indexes[k]] <- current.avg.fare
+  }
+}
+
+data.combined$Ticket.party.size <- ticket.party.size
+data.combined$Avg.fare <- avg.fare
+
+# Refresh 1st class "Mr." dataframe
+first.mr.df <- data.combined[indexes.first.mr, ]
+summary(first.mr.df)
+
+# Visualize new features
+ggplot(first.mr.df[first.mr.df$Survived != "None",], aes(x = Ticket.party.size, fill = Survived)) +
+  geom_density(alpha = 0.5) +
+  ggtitle("Survival Rates 1st Class 'Mr.' by Ticket.party.size")
+
+ggplot(first.mr.df[first.mr.df$Survived != "None",], aes(x = Avg.fare, fill = Survived)) +
+  geom_density(alpha = 0.5) +
+  ggtitle("Survival Rates 1st Class 'Mr.' by Avg.fare")
+
+# Hypothesis - Ticket.party.size is highly correlated with Avg.fare
+summary(data.combined$Avg.fare)
+
+# One missing value, take a look
+data.combined[is.na(data.combined$Avg.fare), ]
+
+# Get records for similar passengers and summarize Avg.fares
+indexes <- with(data.combined, which(Pclass == "3" & Title == "Mr." & Family.size == 1 & Ticket != "3701"))
+similar.na.passengers <- data.combined[indexes,]
+summary(similar.na.passengers$Avg.fare)
+
+# Use median since close to mean and a little higher than mean
+data.combined[is.na(avg.fare), "Avg.fare"] <- 7.840
+
+# Leverage caret's preProcess function to normalize data
+preproc.data.combined <- data.combined[, c("Ticket.party.size", "Avg.fare")]
+preProc <- preProcess(preproc.data.combined, method = c("center", "scale"))
+
+postproc.data.combined <- predict(preProc, preproc.data.combined)
+
+# Hypothesis refuted for all data
+cor(postproc.data.combined$Ticket.party.size, postproc.data.combined$Avg.fare)
+
+# How about for just 1st class all-up?
+indexes <- which(data.combined$Pclass == "1")
+cor(postproc.data.combined$Ticket.party.size[indexes], postproc.data.combined$Avg.fare[indexes])
+# Hypothesis refuted again
+
+# OK, let's see if our feature engineering has made any difference
+features <- c("Pclass", "New.title", "Family.size", "Ticket.party.size", "Avg.fare")
+rpart.train.3 <- data.combined[1:891, features]
+
+# Run CV and check out results 
+rpart.3.cv.1 <- rpart.cv(94622, rpart.train.3, rf.label, ctrl.3)
+rpart.3.cv.1
+
+# Plot
+prp(rpart.3.cv.1$finalModel, type = 0, extra = 1, under = TRUE)
+
+
+
+
+
+
+
+
